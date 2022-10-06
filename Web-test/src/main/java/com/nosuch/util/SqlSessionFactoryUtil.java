8bc41d10771f464399a956e18bfd7f78becbf727
Isaac Zhang <isaac-z@outlook.com>
2022-10-06 14:59:09 +0800

    update

--- Web-test/src/main/java/com/nosuch/util/SqlSessionFactoryUtil.java
+++ Web-test/src/main/java/com/nosuch/util/SqlSessionFactoryUtil.java
@@ -12,7 +12,6 @@ public class SqlSessionFactoryUtil {
     private static SqlSessionFactory sqlSessionFactory;
 
     static {
-        //静态代码块，随着类的加载自动执行，且只执行一次
         try {
             String resource = "mybatis-config.xml";
             InputStream inputStream = Resources.getResourceAsStream(resource);
