532b7aa4276f56da84e2752386d4daa6bbdc47bf
Isaac Zhang <isaac-z@outlook.com>
2022-10-13 12:37:18 +0800

    update

--- springboot/springboot_chatroom/src/main/java/cc/nozuch/controller/MsgController.java
+++ springboot/springboot_chatroom/src/main/java/cc/nozuch/controller/MsgController.java
@@ -5,6 +5,9 @@ import cc.nozuch.serviece.MsgService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 @RestController
 @RequestMapping("/msgs")
 public class MsgController {
@@ -17,8 +20,21 @@ public class MsgController {
         return msgService.getById(id);
     }
 
-    @PostMapping
-    public Boolean save(@RequestBody Msg msg) {
+    @PostMapping("{message}")
+    public Boolean save(@PathVariable String message) {
+        //获取当前时间
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = sdf.format(new java.util.Date());
+        java.util.Date timeDate = null;
+        try {
+            timeDate = sdf.parse(time);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        java.sql.Timestamp timestamp = new java.sql.Timestamp(timeDate.getTime());
+        Msg msg = new Msg();
+        msg.setMessage(message);
+        msg.setTime(timestamp);
         return msgService.save(msg);
     }
 }
