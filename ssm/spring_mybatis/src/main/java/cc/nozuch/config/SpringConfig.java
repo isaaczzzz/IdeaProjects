801b4acb4a5bacbcec61270cd21361527b3e83f5
Isaac Zhang <isaac-z@outlook.com>
2022-10-13 14:22:39 +0800

    update

---
+++ ssm/spring_mybatis/src/main/java/cc/nozuch/config/SpringConfig.java
@@ -0,0 +1,10 @@
+package cc.nozuch.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cc.nozuch")
+public class SpringConfig {
+    
+}
